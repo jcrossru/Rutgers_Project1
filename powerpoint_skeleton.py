a7443285e53795495b6c578b55740da310bc3141
Kseniya Bogoslovskaya <fromkseniya@gmail.com>
2019-03-23 12:55:40 -0400

    Added powepoint_skeletom
    Summarazing the plan for PowerPoint Presentation

---
+++ powerpoint_skeleton.py
@@ -0,0 +1,29 @@
+1) Title Slide -
+    * The name of the project + team members
+
+2) Motivation & Summary Slide -
+    * Describe the hypothesis for our project
+        * Describe the questions we asked, the reason we asked them
+            * Were we able to answer them and briefly summarize our findings
+
+3) Question & Data Slide -
+    * Summarize where and how we found the data (elaborate on the questions we asked)
+        
+        4) Data Cleanup & Exploration Slide -
+            * Describe the exploration and cleanup process
+                * Discuss insights we had while exploring the data that we didn't anticipate
+                * Discuss any problems that arose after exploring the data, and how we resolved them
+                    * Present and discuss interesting figures developed during exploration (using Jupyter)
+
+5)  Data Analysis Slide -
+    * Summarize our conclusion
+        * Describe the analysis process (using Jupyter Notebook)
+        * Data Visualization (plots, etc)
+        
+        6) Discussion Slide
+            * Discuss our findings. Did we find what we expected to find? If not, why not?
+                * General conclusions from our analysis.
+
+7) * Post Mortem Slide
+    * Discuss any difficulties that arose after data expolarion process, and how we dealt with them
+        * Discuss any additional questions that came up, discuss the future analysis plan including the timeline 
